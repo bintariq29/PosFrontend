380fff79a6fd653a6aa9fb13a0450daf90199ebd
bintariq29 <hanzlat478@gmail.com>
2026-02-19 15:02:41 +0500

    file structue for edit and detail

---
+++ src/app/purchase/purchase-detail/purchase-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-detail',
+  imports: [],
+  templateUrl: './purchase-detail.component.html',
+  styles: ``
+})
+export class PurchaseDetailComponent {
+
+}
